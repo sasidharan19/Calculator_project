11b177340a4454457500199bcb7f15ada030669b
Sasidharan Senthilkumar <sasidharan192000@gmail.com>
2024-04-05 18:15:26 +0530

    functions for math operations

---
+++ scripts.js
@@ -0,0 +1,33 @@
+//functions for math operations
+
+let firstNumber = 2;
+let SecondNumber = 4;
+
+//add
+function add(firstNumber, SecondNumber) {
+    let additionResult = firstNumber + SecondNumber;
+    console.log(additionResult);
+}
+
+//subtract
+function subtract(firstNumber, SecondNumber) {
+    let subtractResult = firstNumber - SecondNumber;
+    console.log(subtractResult);
+}
+
+//multiply
+function multiply(firstNumber, SecondNumber) {
+    let multiplyResult = firstNumber * SecondNumber;
+    console.log(multiplyResult);
+}
+
+//divide
+function divide(firstNumber, SecondNumber) {
+    let divisionResult = firstNumber / SecondNumber;
+    console.log(divisionResult);
+}
+
+add(firstNumber, SecondNumber);
+subtract(firstNumber, SecondNumber);
+multiply(firstNumber, SecondNumber);
+divide(firstNumber, SecondNumber);
